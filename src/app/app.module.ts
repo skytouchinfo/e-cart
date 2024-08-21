f670f4abed21b843b550b0a6b15644da0fff43b4
skytouchinfo <webdev826@gmail.com>
2024-08-21 15:47:31 +0530

    move product-list to products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { TopHeaderComponent } from './top-header/top-header.component';
-import { TopMenuComponent } from './header/top-menu/top-menu.component';
-import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { SearchComponent } from './product-list/search/search.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {TopHeaderComponent} from './top-header/top-header.component';
+import {TopMenuComponent} from './header/top-menu/top-menu.component';
+import {MainMenuComponent} from './header/main-menu/main-menu.component';
 import {FormsModule} from "@angular/forms";
-import { ProductsComponent } from './products/products.component';
+import {ProductsComponent} from './products/products.component';
+import {ProductListComponent} from "./products/product-list/product-list.component";
+import {SearchComponent} from "./products/product-list/search/search.component";
+
 
 @NgModule({
   declarations: [
@@ -18,9 +19,9 @@ import { ProductsComponent } from './products/products.component';
     TopHeaderComponent,
     TopMenuComponent,
     MainMenuComponent,
+    ProductsComponent,
     ProductListComponent,
-    SearchComponent,
-    ProductsComponent
+    SearchComponent
   ],
   imports: [
     BrowserModule,
@@ -29,4 +30,5 @@ import { ProductsComponent } from './products/products.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
