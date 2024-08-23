9a60533365aea798628f465740729e51577b541f
skytouchinfo <webdev826@gmail.com>
2024-08-23 12:15:19 +0530

    Add antd zoro to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {TopHeaderComponent} from './top-header/top-header.component';
-import {TopMenuComponent} from './header/top-menu/top-menu.component';
-import {MainMenuComponent} from './header/main-menu/main-menu.component';
-import {FormsModule} from "@angular/forms";
-import {ProductsComponent} from './products/products.component';
-import {ProductComponent} from "./products/product/product.component";
-import {SearchComponent} from "./products/product/search/search.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product/product.component';
+import { SearchComponent } from './products/product/search/search.component';
 import { FilterComponent } from './products/filter/filter.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
-
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -24,14 +32,17 @@ import { FilterComponent } from './products/filter/filter.component';
     ProductsComponent,
     ProductComponent,
     SearchComponent,
-    FilterComponent
+    FilterComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzInputModule,
+    NzButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/products/product/search/search.component.ts
+++ src/app/products/product/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
